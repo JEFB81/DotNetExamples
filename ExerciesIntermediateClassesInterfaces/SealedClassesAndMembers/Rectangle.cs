66af0fcdd4cabc84f93ddb5d4ac6ea6bb3121ecf
J.Fernandez <jhanerfernandez@outlook.com>
2021-09-03 13:30:47 +0200

    Change example sealed

--- ExerciesIntermediateClassesInterfaces/SealedClassesAndMembers/Rectangle.cs
+++ ExerciesIntermediateClassesInterfaces/SealedClassesAndMembers/Rectangle.cs
@@ -1,14 +1,8 @@
 ﻿namespace ExerciesIntermediateClassesInterfaces.SealedClassesAndMembers
 {
-    public class Rectangle : Shape
+    public class Rectangle // : Shape will not work, because of sealed
     {
         public int BorderSize  { get; set; }
-
-        public sealed override void Draw()
-        {
-            // you can write any code specific to the class itself
-
-            System.Console.WriteLine("Draw a Rectangle");
-        }
+         
     }
 }
--- ExerciesIntermediateClassesInterfaces/SealedClassesAndMembers/Shape.cs
+++ ExerciesIntermediateClassesInterfaces/SealedClassesAndMembers/Shape.cs
@@ -1,10 +1,10 @@
 ﻿namespace ExerciesIntermediateClassesInterfaces.SealedClassesAndMembers
-{ 
-    public abstract class Shape
+{
+    sealed public class Shape
     {
-        public int Width { get; set; }
-        public int Height { get; set; } 
-        public abstract void Draw();
+        public void Draw() 
+        {
+        }
          
     }
 }
