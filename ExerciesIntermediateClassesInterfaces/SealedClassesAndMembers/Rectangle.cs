439a32955b521d5cf7bd45d0d899d10a1c78d449
J.Fernandez <jhanerfernandez@outlook.com>
2021-09-03 13:20:40 +0200

    added sealed example

---
+++ ExerciesIntermediateClassesInterfaces/SealedClassesAndMembers/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿namespace ExerciesIntermediateClassesInterfaces.SealedClassesAndMembers
+{
+    public class Rectangle : Shape
+    {
+        public int BorderSize  { get; set; }
+
+        public sealed override void Draw()
+        {
+            // you can write any code specific to the class itself
+
+            System.Console.WriteLine("Draw a Rectangle");
+        }
+    }
+}
---
+++ ExerciesIntermediateClassesInterfaces/SealedClassesAndMembers/Shape.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciesIntermediateClassesInterfaces.SealedClassesAndMembers
+{ 
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; } 
+        public abstract void Draw();
+         
+    }
+}
