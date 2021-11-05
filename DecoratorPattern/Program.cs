a62a586f180947c3fc4dc82961c94612f8ebf9f9
J.Fernandez <jhanerfernandez@outlook.com>
2021-11-05 16:57:55 +0100

    Added Decorator Pattern example

---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DecoratorPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create book
+            var book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+            // Create video
+            var video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            // Make video borrowable, then borrow and display
+            Console.WriteLine("\nMaking video borrowable:");
+            var borrowVideo = new Borrowable(video);
+            borrowVideo.BorrowItem("Customer #1");
+            borrowVideo.BorrowItem("Customer #2");
+            borrowVideo.Display();
+
+            // Make book borrowable, then borrow and display
+            var borrowBook = new Borrowable(book);
+            borrowBook.BorrowItem("Customer #3");
+            borrowBook.Display();
+            
+            // Maak video reorder, the Order and Display
+            var reorderVideo = new Reorder(video);
+            reorderVideo.OrderItem("Jaws");
+            reorderVideo.Display();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DecoratorPattern/Reorder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecoratorPattern
+{
+    /// <summary>
+    /// The 'ConcreteDecoratorB Renewable' class
+    /// </summary>
+    public class Reorder : Decorator
+    {
+        protected readonly List<string> orders = new List<string>();
+
+        // Constructor
+        public Reorder(LibraryItem libraryItem)
+            : base(libraryItem)
+        { }
+
+        public void OrderItem(string name)
+        {
+            orders.Add(name);
+            libraryItem.NumCopies++;
+        }
+        public override void Display()
+        {
+            base.Display();
+
+            foreach (string item in orders)
+            {
+                Console.WriteLine(" Order: " + item);
+            }
+        }
+    }
+}
