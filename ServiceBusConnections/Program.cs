4e1abeab1554e4ae28dd0131ec560271af4d4f74
J.Fernandez <jhanerfernandez@outlook.com>
2021-11-19 18:24:48 +0100

    added topic sender and receiver process

--- ServiceBusConnections/Program.cs
+++ ServiceBusConnections/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
 using ServiceBusConnections;
+using ServiceBusConnections.Topics;
 using System;
 using System.Threading.Tasks;
 
@@ -9,11 +10,21 @@ namespace privatemessagereceiver
     {
         public static void Main(string[] args)
         {
+            // QUEUE
             Console.WriteLine("Sending a message to the Sales Messages queue...");
             MessageSender.SendSalesMessageAsync().GetAwaiter().GetResult();
             Console.WriteLine("Message was sent successfully.");
+            Console.WriteLine("Getting a message from the Sales Messages queue...");
+            // receiver
+            MessageReceiver.ReceiveSalesMessageAsync().GetAwaiter().GetResult();
 
-            //MessageReceiver.ReceiveSalesMessageAsync().GetAwaiter().GetResult();
+            //TOPIC
+            // sender
+            Console.WriteLine("Sending a message to the Sales Performance topic...");
+            TopicMessageSender.SendPerformanceMessageAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Message was sent successfully.");
+            // Receiver
+            TopicMessageReceiver.MainAsync().GetAwaiter().GetResult();
         }
 
 
---
+++ ServiceBusConnections/Topics/TopicMessageSender.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusConnections.Topics
+{
+    public static class TopicMessageSender
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://example.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AbCdEfGhIjKlMnOpQrStUvWxYz==";
+        const string TopicName = "salesperformancemessages"; 
+
+        public static async Task SendPerformanceMessageAsync()
+        {
+            // By leveraging "await using", the DisposeAsync method will be called automatically once the client variable goes out of scope.
+            // In more realistic scenarios, you would want to store off a class reference to the client (rather than a local variable) so that it can be used throughout your program.
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            await using ServiceBusSender sender = client.CreateSender(TopicName);
+
+            try
+            {
+                string messageBody = "Total sales for Brazil in August: $13m.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+    }
+}
